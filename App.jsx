624c037cbde8a765bc5815c922da098643129916
Vikee96 <120468603+Vikee96@users.noreply.github.com>
2023-03-01 13:24:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import  React from "react"
+const App=()=>{
+    return(
+        <>
+        <div>App</div>
+        <h1>hello</h1>
+        <h2>2+2</h2>
+        <p>para {5+0}</p>
+        </>
+    )
+}
+export default App

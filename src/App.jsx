a1f84be636edf427f3020fcd6bc5ca202cc4879d
Vikee96 <vikee539@gmail.com>
2023-03-04 12:07:09 +0530

    pushing the code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from './Components/Nav';
+import "./Components/globle.css"
+const App=()=>{
+    return(
+        <>
+        <Nav/>
+        </>
+    )
+}
+export default App
---
+++ src/Components/Menu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Menu=()=>{
+    return(
+        <>
+           <div className="divlist">
+           <ul>
+            <li>
+              <a href="home">Home</a>  
+            </li>
+            <li>
+                <a href="login">Login</a>
+            </li>
+            <li>
+                <a href="signup">Signup</a>
+            </li>
+            <li>
+                <a href="about">About</a>
+            </li>
+            
+           </ul>
+           
+           </div>
+           </>
+    )
+}
+export default Menu
